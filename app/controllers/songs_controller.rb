debe71772a3838296701e9f4f50bc845b540a05f
idmyn <hello@davidmyno.rs>
2019-10-09 17:48:56 +0100

    added new form for songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,9 +5,23 @@ class SongsController < ApplicationController
         erb :'/songs/index'
     end
 
+    get "/songs/new" do
+      erb :"/songs/new"
+    end
+
     get '/songs/:slug' do
         @song = Song.find_by_slug(params[:slug])
         erb :'/songs/show'
     end
 
+    post "/songs" do
+
+      name = params[:name]
+      artist_name = params[:artist_name]
+      artist =
+        Artist.find_by(name: artist_name) || Artist.create(name: artist_name)
+      @song = Song.create(name: name, artist: artist)
+
+      redirect "/songs/#{@song.slug}"
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,2 @@
 # Add seed data here. Seed your database with `rake db:seed`
-
-
-
-
-
-# create_table "artists", force: :cascade do |t|
-#     t.string "name"
-#   end
-
-#   create_table "songs", force: :cascade do |t|
-#     t.string "name"
-#     t.integer "artist_id"
-#   end
+LibraryParser.parse
